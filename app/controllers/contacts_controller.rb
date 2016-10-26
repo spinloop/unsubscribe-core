7912618041379fc4c49482eece95d00e04432379
Chris Eldridge <cre@mojotech.com>
2016-10-26 14:10:45 -0400

    add contacts controller

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,15 @@
+class ContactsController < ApplicationController
+  def create
+    contact = Contact.new(contact_params)
+    
+    if contact.save
+      render json: contact_params, status: 201
+    else
+      render json: contact.errors.messages, status: 422
+    end
+  end
+
+  private def contact_params
+    params.require(:contact).permit(:first_name, :last_name, :email)
+  end
+end
