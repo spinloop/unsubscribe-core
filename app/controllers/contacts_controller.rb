7774f74f7a6619ce04ac5cf4d6de38c193531337
Chris Eldridge <cre@mojotech.com>
2016-10-27 16:53:00 -0400

    add message to contacts

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,7 @@
 class ContactsController < ApplicationController
   def create
     contact = Contact.new(contact_params)
-    
+
     if contact.save
       render json: contact_params, status: 201
     else
@@ -10,6 +10,6 @@ class ContactsController < ApplicationController
   end
 
   private def contact_params
-    params.require(:contact).permit(:first_name, :last_name, :email)
+    params.require(:contact).permit(:first_name, :last_name, :email, :message)
   end
 end
