faf6b9512aaa848c7a1ff47940708f220afa05e3
Chris Eldridge <cre@mojotech.com>
2016-10-29 07:30:47 -0400

    add heroku email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,14 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  ActionMailer::Base.smtp_settings = {
+    user_name:      ENV['SENDGRID_USERNAME'],
+    password:       ENV['SENDGRID_PASSWORD'],
+    authentication: :plain,
+    address:        'smtp.sendgrid.net',
+    port:           '587',
+    domain:         'heroku.com',
+    enable_starttls_auto: true
+  }
 end
