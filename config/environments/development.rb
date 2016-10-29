f0c41ca686d8da835356d7cb4bdd95fdd20eb1db
Chris Eldridge <cre@mojotech.com>
2016-10-29 10:24:41 -0400

    send development emails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,14 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  ActionMailer::Base.smtp_settings = {
+    user_name:      ENV['SENDGRID_USERNAME'],
+    password:       ENV['SENDGRID_PASSWORD'],
+    authentication: :plain,
+    address:        'smtp.sendgrid.net',
+    port:           '587',
+    domain:         'localhost',
+    enable_starttls_auto: true
+  }
 end
