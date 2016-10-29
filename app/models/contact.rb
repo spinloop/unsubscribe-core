d17681215b93530368e4c7dc62daa529eb7d674e
Chris Eldridge <cre@mojotech.com>
2016-10-29 11:43:08 -0400

    add email validation

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,8 +1,9 @@
 class Contact < ActiveRecord::Base
   validates :first_name,
             :last_name,
-            :email,
             presence: true
+
+  validates :email, email: true, presence: true
 
   def full_name
     "#{self.first_name} #{self.last_name}"
